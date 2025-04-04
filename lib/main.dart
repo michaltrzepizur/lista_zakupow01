ee0b7dc04fbfaf0ca4bf436db174e71880b1d5b5
Michal Trzepizur <stejnel@outlook.com>
2025-04-04 22:00:12 +0200

    Repair Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:lista_zakupow01/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// Klasa MyApp to główny widget aplikacji
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lista zakupów',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ShoppingListsScreen(),
-    );
-  }
-}
-
-class ShoppingListsScreen extends StatelessWidget {
-  const ShoppingListsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Twoje listy zakupów')),
-      body: Center(child: Text('Tu będzie lista zakupów')),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(userName: 'Majkel'),
     );
   }
 }
